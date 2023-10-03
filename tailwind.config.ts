4a9367c0dc0773f02c72a4f65c872d14bc621927
ponta10 <mponta2002@gmail.com>
2023-10-03 14:30:57 +0900

    storybook tailwind導入

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,10 +1,10 @@
 import type { Config } from 'tailwindcss'
-
 const config: Config = {
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/components/**/*.stories.@(js|jsx|ts|tsx)"
   ],
   theme: {
     extend: {
@@ -13,6 +13,9 @@ const config: Config = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      colors: {
+        'base': '#FFA234',
+      }
     },
   },
   plugins: [],
