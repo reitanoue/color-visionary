e77a0151286a3393154aad7d92ac0b1aa724230c
miura kota <94046278+ponta10@users.noreply.github.com>
2023-10-03 18:28:15 +0900

    buttonコンポーネント作成 (#2)
    * 不要ディレクトリ削除
    
    * storybook tailwind導入
    
    * atoms buttonコンポーネント作成
    
    * lint修正
    
    * yml修正
    
    * ybabel設定変更
    
    * yarn upgrade
    
    * nodeバージョン指定
    
    * Nodeバージョン更新
    
    * yam修正
    
    * jset設定変更
    
    * yml再修正
    
    * jest設定変更
    
    * jest設定変更
    
    * jest設定再変更
    
    * jest設定再再変更
    
    * jest設定修正
    
    * jest設定再修正
    
    * jest設定再再修正
    
    * テスト
    
    * lintのみ

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,20 +1,53 @@
-import type { StorybookConfig } from '@storybook/react-vite'
+import type { StorybookConfig } from '@storybook/nextjs'
+const path = require('path')
 
 const config: StorybookConfig = {
-  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+  stories: ['../src/components/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
     '@storybook/addon-onboarding',
     '@storybook/addon-interactions',
-    '@storybook/react-vite/preset',
+    '@storybook/addon-actions',
+    {
+      name: '@storybook/addon-postcss',
+      options: {
+        postcssLoaderOptions: {
+          implementation: require('postcss'),
+        },
+      },
+    },
   ],
   framework: {
-    name: '@storybook/react-vite',
+    name: '@storybook/nextjs',
     options: {},
   },
   docs: {
     autodocs: 'tag',
+  },
+  webpackFinal: async (config) => {
+    config.resolve = config.resolve || {}
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@/components': path.resolve(__dirname, '../src/components'),
+      '@/i18n': path.resolve(__dirname, '../i18n'),
+      'next-i18next': 'react-i18next',
+    }
+    config.module?.rules?.push({
+      test: /\.css$/,
+      use: [
+        {
+          loader: 'postcss-loader',
+          options: {
+            postcssOptions: {
+              plugins: [require('tailwindcss'), require('autoprefixer')],
+            },
+          },
+        },
+      ],
+      include: path.resolve(__dirname, '../'),
+    })
+    return config
   },
 }
 export default config
